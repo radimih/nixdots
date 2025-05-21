9bbbd7b6f271d8a948ef53935646c105da5cabaf
Radimir Mikhailov <radimir.com@gmail.com>
2025-05-21 11:31:57 +0700

    Выделена настройка локалей в отдельный файл

--- hosts/_common/default.nix
+++ hosts/_common/default.nix
@@ -9,30 +9,16 @@
 {
   imports = [
     ./home-manager.nix
+    ./locale.nix
     ./nix.nix
     ./users.nix
   ];
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "ru_RU.UTF-8";
-      LC_IDENTIFICATION = "ru_RU.UTF-8";
-      LC_MEASUREMENT = "ru_RU.UTF-8";
-      LC_MONETARY = "ru_RU.UTF-8";
-      LC_NAME = "ru_RU.UTF-8";
-      LC_NUMERIC = "ru_RU.UTF-8";
-      LC_PAPER = "ru_RU.UTF-8";
-      LC_TELEPHONE = "ru_RU.UTF-8";
-      LC_TIME = "ru_RU.UTF-8";
-    };
-  };
-
-  time.timeZone = globalSpec.timeZone;
 
   networking.networkmanager.enable = true;
 
   nixpkgs.config.allowUnfree = true;
 
   system.stateVersion = globalSpec.stateVersion;
+
+  time.timeZone = globalSpec.timeZone;
 }
