ed16a16cbec328a897021a08e1ed0297114a8bf5
Radimir Mikhailov <radimir.com@gmail.com>
2025-06-02 13:11:06 +0700

    Добавлена минимальная конфигурация fish

--- hosts/_common/users.nix
+++ hosts/_common/users.nix
@@ -5,11 +5,25 @@
 }:
 
 {
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting
+      fish_vi_key_bindings
+      set fish_cursor_default      block
+      set fish_cursor_insert       line
+      set fish_cursor_replace_one  underscore
+      set fish_cursor_replace      underscore
+      set fish_cursor_visual       block
+      set fish_cursor_external     line
+    '';
+  };
+
   users = {
     defaultUserShell = pkgs.fish;
     users.${globalSpec.admin.name} = {
-      isNormalUser = true;
       description = globalSpec.admin.desc;
+      isNormalUser = true;
       extraGroups = [
         "networkmanager"
         "wheel"
