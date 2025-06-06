221dc9b9c291f5417e2ecf7b4b16c8c21cbc726a
Radimir Mikhailov <radimir.com@gmail.com>
2025-06-06 12:43:22 +0700

    Поправлено форматирование по стандарту

--- users/radimir.nix
+++ users/radimir.nix
@@ -8,9 +8,9 @@
 
 {
   /*
-  Нет необходимости устанавливать параметры home.username|homeDirectory, так как Home Manager
-  в режиме NixOS-модуля устанавливает их автоматически:
-  https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L49-L50
+    Нет необходимости устанавливать параметры home.username|homeDirectory, так как
+    Home Manager в режиме NixOS-модуля устанавливает их автоматически:
+    https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L49-L50
   */
 
   home.packages = with pkgs; [
