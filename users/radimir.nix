86ba88c249ab16d25d914c11fc914cabe867b176
Radimir Mikhailov <radimir.com@gmail.com>
2025-06-06 12:34:24 +0700

    Добавлены пояснения

--- users/radimir.nix
+++ users/radimir.nix
@@ -7,6 +7,12 @@
 }:
 
 {
+  /*
+  Нет необходимости устанавливать параметры home.username|homeDirectory, так как Home Manager
+  в режиме NixOS-модуля устанавливает их автоматически:
+  https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L49-L50
+  */
+
   home.packages = with pkgs; [
     wl-clipboard
   ];
