a222ff015c14c36f7b5baf90be39ec26b5349a2b
Radimir Mikhailov <radimir.com@gmail.com>
2025-08-22 14:59:48 +0700

    optional hardware-configuration.nix file

--- parts/flake/nixosConfigurations.nix
+++ parts/flake/nixosConfigurations.nix
@@ -1,5 +1,3 @@
-# https://github.com/henrysipp/nix-setup/blob/48a93d0275eba0adf48977609fc100dce8f9b49c/modules/flake/host-machines.nix
-
 {
   config,
   inputs,
@@ -10,21 +8,22 @@
 
 {
   flake.nixosConfigurations = {
-    vm-test = inputs.nixpkgs.lib.nixosSystem {
-      modules = [
-        {
-          networking.hostName = "vm-test";
-          nixpkgs.config.allowUnfree = true;
-        }
-        # хосты как flake-parts-модули (рабочий вариант)
-        ../hosts/vm-test/hardware-configuration.nix
-        config.flake.modules.nixos.host-vm-test
-      ];
-      specialArgs = {
-        inherit inputs;
-        inherit self;
+    vm-test =
+      let
+        hardConfFile = ../hosts/vm-test/hardware-configuration.nix;
+      in
+      inputs.nixpkgs.lib.nixosSystem {
+        modules = [
+          {
+            networking.hostName = "vm-test";
+            nixpkgs.config.allowUnfree = true;
+          }
+          config.flake.modules.nixos.host-vm-test
+        ] ++ lib.optional (builtins.pathExists hardConfFile) hardConfFile;
+        specialArgs = {
+          inherit inputs;
+          inherit self;
+        };
       };
-    };
   };
-
 }
