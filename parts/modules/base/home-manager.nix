3c52a826947823d40231c194549a72d9eb2a7cb4
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-02 16:08:42 +0700

    user 3

--- parts/modules/base/home-manager.nix
+++ parts/modules/base/home-manager.nix
@@ -8,7 +8,7 @@
 }:
 
 {
-  flake.modules.nixos.base = {
+  flake.modules.nixos.base = {config', ...}: {
 
     imports = [
       inputs.home-manager.nixosModules.home-manager
@@ -28,7 +28,7 @@
         home.stateVersion = "25.05";  # TODO: stateVersion: 1) одинаково для всех пользователей? 2) вынести в глобальные константы?
         programs.home-manager.enable = true;
       }
-      config.flake.modules.homeManager.host-vm-test
+      config.flake.modules.homeManager."host-${config'.networking.hostName}"
       config.flake.modules.homeManager.user-radimir
       # config.flake.modules.homeManager.base
     ];
