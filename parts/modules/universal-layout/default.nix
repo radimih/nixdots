cfbb7c65070828f865c2a9badf85e8251e98624b
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-03 14:47:09 +0700

    fix

--- parts/modules/universal-layout/default.nix
+++ parts/modules/universal-layout/default.nix
@@ -1,6 +1,6 @@
 # Universal Layout like the https://github.com/braindefender/universal-layout";
 {
-  flake.modules.nixos.base =
+  flake.modules.nixos.universal-layout =
     { lib, ...}:
     {
       # TODO: названия раскладок через опции
