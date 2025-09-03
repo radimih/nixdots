30203f00917dab8fe8f30ed0a9538f6ad60bafd4
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-03 10:57:51 +0700

    fix

--- parts/users/radimir.nix
+++ parts/users/radimir.nix
@@ -23,6 +23,7 @@ in
       home-manager.users.${user.name}.imports = [
         config.flake.modules.homeManager.base
         config.flake.modules.homeManager.user-radimir
+      ];
     };
 
   flake.modules.homeManager."user-${user.name}" = {
