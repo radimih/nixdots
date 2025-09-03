55e9e444ea819aa3a578da8f3b8ed59800f6a5b7
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-03 20:12:55 +0700

    trial 2

--- parts/hosts/vm-test/default.nix
+++ parts/hosts/vm-test/default.nix
@@ -40,7 +40,7 @@ in
         niri
       ] ++ [
         {
-          home.file."hello-host.txt".text = "Привет, radimir! From the ${host} host.";
+          home.file."hello-host.txt".text = "Привет, radimir! From the ${host} host. Double";
         }
       ];
     };
