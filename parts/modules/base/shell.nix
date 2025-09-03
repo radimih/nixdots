0558fe32722e0a4e036a9fbb78cffd90a42d47eb
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-03 13:08:01 +0700

    fix

--- parts/modules/base/shell.nix
+++ parts/modules/base/shell.nix
@@ -4,24 +4,25 @@
 }:
 
 {
-  flake.modules.nixos.base = {
+  flake.modules.nixos.base =
+    { pkgs, ...}:
+    {
+      programs.fish = {
+        enable = true;
+        interactiveShellInit = ''
+          set fish_greeting
+          fish_vi_key_bindings
+          set fish_cursor_default      block
+          set fish_cursor_insert       line
+          set fish_cursor_replace_one  underscore
+          set fish_cursor_replace      underscore
+          set fish_cursor_visual       block
+          set fish_cursor_external     line
+        '';
+      };
 
-    programs.fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set fish_greeting
-        fish_vi_key_bindings
-        set fish_cursor_default      block
-        set fish_cursor_insert       line
-        set fish_cursor_replace_one  underscore
-        set fish_cursor_replace      underscore
-        set fish_cursor_visual       block
-        set fish_cursor_external     line
-      '';
-    };
-
-    users = {
-      defaultUserShell = pkgs.fish;
-    };
+      users = {
+        defaultUserShell = pkgs.fish;
+      };
   };
 }
