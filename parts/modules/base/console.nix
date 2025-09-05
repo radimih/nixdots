bc8f62aaadbcb549d47b98cf491d64370504cc7d
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-05 12:58:45 +0700

    trial 1

--- parts/modules/base/console.nix
+++ parts/modules/base/console.nix
@@ -4,7 +4,7 @@
     {
       # Выводить в консоль только сообщения ядра уровнем меньше 3
       # (4 - warning, 3 - error, 2 - critical, 1- alert, 0 - emergy)
-      # boot.consoleLogLevel = 3;
+      boot.consoleLogLevel = 3;
       # Установить шрифт для текстовой консоли (/dev/ttyN) с поддержкой кириллицы
       console = {
         font = "ter-u16n";  # "UniCyr_8x16" - вариант шрифта из стандартной поставки
--- parts/modules/boot/faster.nix
+++ parts/modules/boot/faster.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.boot =
+  flake.modules.nixos.boot2 =
     { lib, ...}:
     {
       # Использовать systemd при загрузке с initrd
