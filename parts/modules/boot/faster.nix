4aff01f280dc735e8492bd47740955db585cc5f3
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-07 14:06:00 +0000

    trial

--- parts/modules/boot/faster.nix
+++ parts/modules/boot/faster.nix
@@ -6,7 +6,7 @@
       boot.initrd.systemd.enable = true;
 
       # Время в секундах на ожидание выбора поколения NixOS (default = 5)
-      boot.loader.timeout = 1;
+      # boot.loader.timeout = 1;
 
       # Не ждать при загрузке готовности сети
       systemd.services.NetworkManger-wait-online.wantedBy = lib.mkForce [];
