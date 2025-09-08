3bf9090425dec434201b93849959e9eb2d00ae3d
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-08 16:48:50 +0700

    trial

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "My Nixos configuration flake";
 
   nixConfig = {
+    accept-flake-config = true;
+    allow-import-from-derivation = false;
     extra-experimental-features = [
       "flakes"
       "nix-command"
       "pipe-operators"
     ];
     extra-substituters = [
-      "https://lanzaboote.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
--- parts/modules/base/nix.nix
+++ parts/modules/base/nix.nix
@@ -5,6 +5,9 @@
       # Для работы flakes нужен git
       environment.systemPackages = [ pkgs.git ];
 
+      # TODO: https://github.com/OkashiOdayakana/nixos-config/blob/main/modules/nixos/core/nix.nix
+      #       https://nix.dev/manual/nix/2.18/command-ref/conf-file
+
       nix = {
         channel.enable = false;
         settings = {
