4a24e598b32e8c802a659188e9df33bf7a7f073b
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-08 15:15:23 +0700

    nixconfig

--- flake.nix
+++ flake.nix
@@ -7,6 +7,12 @@
       "nix-command"
       "pipe-operators"
     ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   inputs = {
