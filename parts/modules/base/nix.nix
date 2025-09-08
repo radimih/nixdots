c28db286a87ec14f4e352fbf1d124f174a230328
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-08 14:52:22 +0700

    nix-community cache

--- parts/modules/base/nix.nix
+++ parts/modules/base/nix.nix
@@ -5,16 +5,23 @@
       # Для работы flakes нужен git
       environment.systemPackages = [ pkgs.git ];
 
-      nix.channel.enable = false;
-
-      nix.settings = {
-        auto-optimise-store = true;
-        experimental-features = [
-          "flakes"
-          "nix-command"
-          "pipe-operators"
-        ];
-        warn-dirty = false;
+      nix = {
+        channel.enable = false;
+        settings = {
+          auto-optimise-store = true;
+          experimental-features = [
+            "flakes"
+            "nix-command"
+            "pipe-operators"
+          ];
+          substituters = [
+            "https://nix-community.cachix.org"
+          ];
+          trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+          warn-dirty = false;
+        };
       };
 
       nixpkgs.config.allowUnfree = true;
