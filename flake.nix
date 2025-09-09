8904670460410f2e4a745ab4b1718870211c9466
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-09 16:06:21 +0700

    switch to stable branches

--- flake.nix
+++ flake.nix
@@ -9,11 +9,9 @@
       "pipe-operators"
     ];
     extra-substituters = [
-      "https://lanzaboote.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
@@ -22,8 +20,7 @@
 
     # --- nixpkgs
 
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     # --- Nix/NixOS infra
 
@@ -35,8 +32,7 @@
     import-tree.url = "github:vic/import-tree";
 
     home-manager = {
-      # url = "github:nix-community/home-manager/release-25.05";
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
