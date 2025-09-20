eaa35e67f9f56123c306a58ccd21af14fc1fe1c7
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-20 19:32:45 +0000

    add adi1090x themes

--- parts/modules/base/boot/visual.nix
+++ parts/modules/base/boot/visual.nix
@@ -1,5 +1,23 @@
 {
+  # https://github.com/adi1090x/plymouth-themes
+  # Список тем: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ad/adi1090x-plymouth-themes/shas.nix
+
+  flake.modules.nixos.boot-visual-adi1090x =
+    { pkgs, ...}:
+    {
+      boot.plymouth =
+      let
+        theme = "circle_hud";
+      in
+      {
+        enable = true;
+        inherit theme;
+        themePackages = [ (pkgs.adi1090x-plymouth-themes.override {selected_themes = [theme];}) ];
+      };
+    };
+
   # https://github.com/catppuccin/plymouth
+
   flake.modules.nixos.boot-visual-catppuccin =
     { pkgs, ...}:
     {
@@ -11,6 +29,7 @@
     };
 
   # Медленное вращение чуть увеличенного чёрно-белого логотипа NixOS
+
   flake.modules.nixos.boot-visual-nixos-rotate =
     { pkgs, ...}:
     {
