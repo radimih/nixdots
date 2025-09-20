d6f905e336ebe243f254430f15ced448e9362277
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-20 18:07:24 +0000

    initial

--- parts/modules/base/boot/visual.nix
+++ parts/modules/base/boot/visual.nix
@@ -1,8 +1,11 @@
 {
-  flake.modules.nixos.boot-visual = {
-
-    boot = {
-      plymouth.enable = true;
+  flake.modules.nixos.boot-visual =
+    { pkgs, ...}:
+    {
+      boot.plymouth = {
+        enable = true;
+        theme = "nixos-bgrt";
+        themePackages = [ pkgs.nixos-bgrt-plymouth ];
+      };
     };
-  };
 }
