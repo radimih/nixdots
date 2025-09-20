dfb27ea16006d07a6f6e9b03f01c54f17bd910d6
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-20 14:39:04 +0000

    initial

---
+++ parts/modules/base/fish.nix
@@ -0,0 +1,18 @@
+{
+  flake.modules.nixos.base = {
+
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+        fish_vi_key_bindings
+        set fish_cursor_default      block
+        set fish_cursor_insert       line
+        set fish_cursor_replace_one  underscore
+        set fish_cursor_replace      underscore
+        set fish_cursor_visual       block
+        set fish_cursor_external     line
+      '';
+    };
+  };
+}
---
+++ parts/modules/base/users.nix
@@ -0,0 +1,11 @@
+# Общие настройки для всех пользователей
+{
+  flake.modules.nixos.base =
+    { pkgs, ...}:
+    {
+      users = {
+        defaultUserShell = pkgs.fish;
+        mutableUsers = false;
+      };
+    };
+}
