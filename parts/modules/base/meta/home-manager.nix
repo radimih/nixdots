82d7632feab77c952ef1123de990a39967b7a1eb
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-21 16:36:36 +0700

    Упрощенa настройка home-manager через sharedModules (#12)

--- parts/modules/base/meta/home-manager.nix
+++ parts/modules/base/meta/home-manager.nix
@@ -2,7 +2,6 @@
 # https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nixos-module
 {
   inputs,
-  config,
   ...
 }:
 {
@@ -16,32 +15,23 @@
       extraSpecialArgs = {
         inherit inputs;
       };
+      sharedModules = [
+        {
+          /*
+            Нет необходимости устанавливать параметры home.username|homeDirectory, так как
+            Home Manager в режиме NixOS-модуля устанавливает их автоматически:
+            https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L53-L54
+          */
+          # example: https://github.com/henrysipp/nix-setup/blob/48a93d0275eba0adf48977609fc100dce8f9b49c/modules/base/system/default.nix
+          home.stateVersion = "25.05";  # TODO: stateVersion: 1) одинаково для всех пользователей? 2) вынести в глобальные константы?
+
+          # Let home Manager install and manage itself
+          programs.home-manager.enable = true;
+          };
+        }
+      ];
       useGlobalPkgs = true;
       useUserPackages = true;
     };
-  };
-
-  flake.modules.homeManager.base = {
-
-    /*
-      Нет необходимости устанавливать параметры home.username|homeDirectory, так как
-      Home Manager в режиме NixOS-модуля устанавливает их автоматически:
-      https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L53-L54
-    */
-
-    # example: https://github.com/henrysipp/nix-setup/blob/48a93d0275eba0adf48977609fc100dce8f9b49c/modules/base/system/default.nix
-    home.stateVersion = "25.05";  # TODO: stateVersion: 1) одинаково для всех пользователей? 2) вынести в глобальные константы?
-
-    # Let home Manager install and manage itself
-    programs.home-manager.enable = true;
-
-    # TODO: https://home-manager-options.extranix.com/?query=autoExpire&release=release-25.05
-    # services = {
-    #   home-manager.autoExpire = {
-    #     enable = true;
-    #     frequency = "weekly";
-    #     store.cleanup = true;
-    #   };
-    # };
   };
 }
