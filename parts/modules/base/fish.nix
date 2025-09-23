d65b814351a1a45b8df30a16ef082f269e869b3d
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-23 14:31:43 +0700

    Отсортированы параметры fish

--- parts/modules/base/fish.nix
+++ parts/modules/base/fish.nix
@@ -4,14 +4,14 @@
     programs.fish = {
       enable = true;
       interactiveShellInit = ''
-        set fish_greeting
         fish_vi_key_bindings
+        set fish_greeting
         set fish_cursor_default      block
-        set fish_cursor_insert       line
-        set fish_cursor_replace_one  underscore
-        set fish_cursor_replace      underscore
-        set fish_cursor_visual       block
         set fish_cursor_external     line
+        set fish_cursor_insert       line
+        set fish_cursor_replace      underscore
+        set fish_cursor_replace_one  underscore
+        set fish_cursor_visual       block
       '';
     };
   };
