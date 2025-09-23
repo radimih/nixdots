98d2ca61f4eafe96f3e22f489a6ee83a7b83af19
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-23 17:23:28 +0700

    initial

---
+++ parts/modules/shell/fish.nix
@@ -0,0 +1,25 @@
+{
+  flake.modules.homeManager.fish =
+    { pkgs, ...}:
+    {
+      programs.fish = {
+        enable = true;
+        interactiveShellInit = ''
+          fish_vi_key_bindings
+          set fish_greeting
+          set fish_cursor_default      block
+          set fish_cursor_external     line
+          set fish_cursor_insert       line
+          set fish_cursor_replace      underscore
+          set fish_cursor_replace_one  underscore
+          set fish_cursor_visual       block
+        '';
+        plugins = with pkgs.fishPlugins; [
+          {
+            name = "autopair";
+            src = autopair.src;
+          }
+        ];
+      };
+    };
+}
