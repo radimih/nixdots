7bc0e16e3acdd558f107552f3a01e9c89ff9aa22
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-23 18:23:29 +0700

    add plugin

--- parts/modules/shell/fish.nix
+++ parts/modules/shell/fish.nix
@@ -14,11 +14,22 @@
           set fish_cursor_replace_one  underscore
           set fish_cursor_visual       block
         '';
-        plugins = with pkgs.fishPlugins; [
-          {
-            name = "autopair";
-            src = autopair.src;
-          }
+        # plugins = with pkgs.fishPlugins; [
+        #   {
+        #     name = "autopair";
+        #     src = autopair.src;
+        #   }
+        # ];
+      plugins =
+        let
+          plug = name: {
+            inherit name;
+            src = pkgs.fishPlugins.${name}.src;
+          };
+        in
+        [
+          (plug "autopair")  # https://github.com/jorgebucaran/autopair.fish
+          (plug "puffer")  # https://github.com/nickeb96/puffer-fish
         ];
       };
     };
