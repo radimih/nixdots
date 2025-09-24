15db73c92f9555d81eb377ad7a2bffca135e94fc
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-24 16:28:55 +0700

    copy/paste keyt bindings

--- parts/modules/programs/kitty/default.nix
+++ parts/modules/programs/kitty/default.nix
@@ -4,6 +4,12 @@
 
     programs.kitty = {
       enable = true;
+      # https://sw.kovidgoyal.net/kitty/actions/
+      keybindings = {
+        "ctrl+c" = "copy_and_clear_or_interrupt";
+        "ctrl+v" = "paste_selection_or_clipboard";
+        "ctrl+shift+v" = "show_kitty_env_vars";
+      };
       settings = {
         cursor_trail = 1;
       };
