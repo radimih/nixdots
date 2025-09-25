d7187329977ff42e6516bc337c6ff9fb145934a7
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-25 11:03:41 +0700

    add theme

--- parts/modules/desktop/stylix.nix
+++ parts/modules/desktop/stylix.nix
@@ -5,12 +5,16 @@
 {
   flake.modules.nixos.desktop =
   { pkgs, ... }:
+  let
+    theme = "solarized-dark";
+  in
   {
     imports = [
       inputs.stylix.nixosModules.stylix
     ];
 
     stylix = {
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
       enable = true;
       opacity.terminal = 0.85;
     };
