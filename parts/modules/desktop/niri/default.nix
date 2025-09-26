2bd6b2fab1de805f6c4cb5ea1ff86805cc1ee6fe
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-26 12:42:33 +0700

    trial

--- parts/modules/desktop/niri/default.nix
+++ parts/modules/desktop/niri/default.nix
@@ -56,33 +56,24 @@
 
           input.keyboard.xkb.layout = "${osConfig.services.xserver.xkb.layout}";
 
-	        layer-rules = [
-            {
-              # Параметры для background-слоя. Название слоя зависит от wallpaper-утилиты.
-              # Для swaybg это 'wallpaper'. Посмотреть доступные слои: niri msg layers
-              matches = [
-                { namespace ="^wallpaper$"; }
-              ];
-              place-within-backdrop = true;
-            }
-          ];
-
-          layout = {
-            background-color = "transparent";
-          };
-
-          outputs.Virtual-1 = {
-            background-color = "transparent";
-          };
-
-	        # window-rules = [
+	        # layer-rules = [
           #   {
+          #     # Параметры для background-слоя. Название слоя зависит от wallpaper-утилиты.
+          #     # Для swaybg это 'wallpaper'. Посмотреть доступные слои: niri msg layers
           #     matches = [
-          #       { app-id ="kitty"; }
+          #       { namespace ="^wallpaper$"; }
           #     ];
-          #     opacity = 0.85;
+          #     place-within-backdrop = true;
           #   }
           # ];
+
+          # layout = {
+          #   background-color = "transparent";
+          # };
+
+          # outputs.Virtual-1 = {
+          #   background-color = "transparent";
+          # };
 
           spawn-at-startup = [
             { command = ["waybar"]; }
