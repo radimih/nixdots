b378fec67a39e94da1f870fc180fa0eed514151f
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-26 11:50:30 +0700

    try env

--- parts/modules/desktop/niri/default.nix
+++ parts/modules/desktop/niri/default.nix
@@ -50,12 +50,9 @@
           };
 
           environment = {
-            DISPLAY = ":0";
+            # DISPLAY = ":0";
             ELECTRON_OZONE_PLATFORM_HINT = "auto";
             NIXOS_OZONE_WL = "1";
-            # XDG_CURRENT_DESKTOP = "niri";
-            # XDG_SESSION_DESKTOP = "niri";
-            # XDG_SESSION_TYPE = "wayland";
           };
 
           input.keyboard.xkb.layout = "${osConfig.services.xserver.xkb.layout}";
