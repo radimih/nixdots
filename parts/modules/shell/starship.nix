b34574041ebe751aa0f6837b347c479a4d79f25e
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-29 13:52:09 +0700

    trial

--- parts/modules/shell/starship.nix
+++ parts/modules/shell/starship.nix
@@ -13,6 +13,7 @@
           fish_style_pwd_dir_length = 3;
           read_only = " ";
           repo_root_style = "pale blue";
+          repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style) ";
           # truncation_length = 5;
           # truncation_symbol = "… /";
         };
