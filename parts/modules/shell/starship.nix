9edb2af9b827f5a257cd77ae7e612e6c711935a6
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-30 15:09:11 +0700

    trial

--- parts/modules/shell/starship.nix
+++ parts/modules/shell/starship.nix
@@ -6,15 +6,21 @@
       enable = true;
       settings = {
         format = builtins.concatStringsSep "" [
-          "$username"
-          "$hostname"
+          "[░▒▓](#a3aed2)"
+          "[  ](bg:#a3aed2 fg:#090c0c)"
+          # "$username"
+          # "$hostname"
           "$directory"
+          "[](fg:#769ff0 bg:#394260)"
           "$git_branch"
           "$git_state"
           "$git_status"
+          "[](fg:#394260 bg:#212736)"
           "$kubernetes"
           "$nix_shell"
+          "[](fg:#212736 bg:#1d2230)"
           "$cmd_duration"
+          "[ ](fg:#1d2230)"
           "\n"
           "$shlvl"
           "$character"
