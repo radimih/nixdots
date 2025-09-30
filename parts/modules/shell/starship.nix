fe6bf18e9f8506fcbb9f1e9ec72a22e9c6ade917
Radimir Mikhailov <radimir.com@gmail.com>
2025-09-30 15:40:11 +0700

    read_only

--- parts/modules/shell/starship.nix
+++ parts/modules/shell/starship.nix
@@ -10,15 +10,15 @@
           # "$username"
           # "$hostname"
           "$directory"
-          "[](fg:#769ff0 bg:#394260)"
+          "[](fg:#769ff0 bg:#394260)"  # )
           "$git_branch"
           "$git_status"
-          "[](fg:#394260 bg:#212736)"
+          "[](fg:#394260 bg:#212736)"  # )
           "$kubernetes"
           "$nix_shell"
-          "[](fg:#212736 bg:#1d2230)"
+          "[](fg:#212736 bg:#1d2230)"  # )
           "$cmd_duration"
-          "[ ](fg:#1d2230)"
+          "[ ](fg:#1d2230)"  # )
           "\n"
           "$shlvl"
           "$character"
@@ -26,9 +26,11 @@
         cmd_duration = {
           min_time = 2 * 1000;  # 2 секунды
           format = "[󰚭 $duration]($style)";
+          style = "";
         };
         directory = {
           read_only = " ";
+          read_only_style = "bg:#769ff0 red";
           repo_root_format = " [$repo_root]($repo_root_style) [$path]($style)[$read_only]($read_only_style) ";
           repo_root_style = "bg:#a3aed2 fg:#090c0c";
           style = "fg:#e3e5e5 bg:#769ff0";
