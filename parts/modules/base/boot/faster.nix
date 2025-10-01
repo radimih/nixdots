7fdfa2cdf2cba1e5297649ab503bd79a5458ae27
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-01 17:41:48 +0700

    Добавлена ТУДУшка про NixOS 25.11

--- parts/modules/base/boot/faster.nix
+++ parts/modules/base/boot/faster.nix
@@ -3,6 +3,7 @@
     { lib, ...}:
     {
       # Использовать systemd при загрузке с initrd
+      # TODO: в 25.11 планируется сделать по-умолчанию: https://github.com/NixOS/nixpkgs/pull/435781
       boot.initrd.systemd.enable = true;
 
       # Время в секундах на ожидание выбора поколения NixOS (default = 5)
