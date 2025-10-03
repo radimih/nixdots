74c4d698c4fba4d88b24ab09fff991811adc935f
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-03 18:21:36 +0700

    trial opacity

--- parts/modules/desktop/niri/rules.nix
+++ parts/modules/desktop/niri/rules.nix
@@ -19,7 +19,7 @@
       window-rules = [
         # Чтобы работала прозрачность (opacity) окон
         # FIXME: возможно ограничить только для kitty
-        { draw-border-with-background = false; }
+        # { draw-border-with-background = false; }
 
 	      # Сделать скруглёнными все углы всех окон
         {
