a43cd0a1d4dc2e5264a0930abb3d74ae0b4bdd17
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-06 10:44:34 +0700

    trial

--- parts/modules/desktop/waybar/style.nix
+++ parts/modules/desktop/waybar/style.nix
@@ -4,7 +4,8 @@
 
     programs.waybar.style = ''
       window#waybar, tooltip {
-          background: alpha(#000000, 0.5);
+          /* background: alpha(#000000, 0.5); */
+          background: 0; */
       }
     '';
 
