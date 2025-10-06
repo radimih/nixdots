35743d30793861d12b5468aa8890593d6ab8bc4f
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-06 13:07:50 +0700

    trial font

--- parts/modules/desktop/waybar/style.nix
+++ parts/modules/desktop/waybar/style.nix
@@ -15,7 +15,7 @@
 
     programs.waybar.style = ''
       * {
-          font-family: 'JetBrains Mono Nerd Font Mono'
+          font-family: "JetBrainsMonoNFM";
           font-size: 11pt;
       }
       window#waybar {
