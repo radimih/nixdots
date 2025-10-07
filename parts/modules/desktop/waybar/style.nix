d2629183e3a11a2753754e2a11f1ac7a3cd29fc1
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-07 13:08:07 +0700

    colors

--- parts/modules/desktop/waybar/style.nix
+++ parts/modules/desktop/waybar/style.nix
@@ -1,40 +1,45 @@
 # https://github.com/Alexays/Waybar/wiki/Styling
+# Стили по-умолчанию: https://github.com/Alexays/Waybar/blob/master/resources/style.css
 {
   flake.modules.homeManager.waybar = {
 
     # CSS Properties: https://docs.gtk.org/gtk3/css-properties.html
 
     programs.waybar.style = ''
-      @define-color fg-color-primary #ffffff;
+      @define-color bg-primary #000000;
+      @define-color fg-clock-msk #707070;
+      @define-color fg-primary #ffffff;
+      @define-color fg-title: #3498db;
 
       * {
+        border: none;
+        border-radius: 0;
         font-family: "RobotoMono Nerd Font Mono";
         font-size: 12pt;
       }
 
       window#waybar {
-        background: #000000;
+        background: @bg-primary;
+        color: @fg-primary;
       }
       window#waybar #window {
-        color: #3498db;
+        color: @fg-title;
       }
 
       tooltip {
-        background: alpha(#000000, 0.8);
+        background: alpha(@bg-primary, 0.8);
       }
 
       #clock.msk {
-        color: #707070;
+        color: @fg-clock-msk;
       }
 
       #workspaces button {
-        border-radius: 0;
         border-bottom: 3px solid transparent;
       }
 
       #workspaces button.focused {
-        border-radius: 0;
-        border-bottom: 3px solid @fg-color-primary;
+        border-bottom: 3px solid @fg-primary;
       }
     '';
 
