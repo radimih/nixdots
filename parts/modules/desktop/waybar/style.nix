5d4c21ac0538104d66b79b2586e717ad64d7d5e7
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-07 12:40:39 +0700

    trial

--- parts/modules/desktop/waybar/style.nix
+++ parts/modules/desktop/waybar/style.nix
@@ -5,24 +5,34 @@
     # CSS Properties: https://docs.gtk.org/gtk3/css-properties.html
 
     programs.waybar.style = ''
+      @define-color fg-color-primary #ffffff;
+
       * {
         font-family: "RobotoMono Nerd Font Mono";
         font-size: 12pt;
       }
+
       window#waybar {
         background: #000000;
       }
       window#waybar #window {
         color: #3498db;
       }
+
       tooltip {
         background: alpha(#000000, 0.8);
       }
+
       #clock.msk {
         color: #707070;
       }
+
+      #workspaces button {
+        border-bottom: 3px solid transparent;
+      }
+
       #workspaces button.focused {
-        color: #e2c43c;
+        border-bottom: 3px solid @fg-color-primary;
       }
     '';
 
