7980b36ff3d43056ac09bf9c8cc53dd88435164d
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-07 18:24:18 +0700

    trial

--- parts/modules/services/universal-layout/default.nix
+++ parts/modules/services/universal-layout/default.nix
@@ -9,12 +9,12 @@
         layout = "En,Ru";
         extraLayouts = {
           En = {
-            description = "en";
+            description = "English Universal";
             languages = [ "eng" ];
             symbolsFile = ./universal-en.xkb;
           };
           Ru = {
-            description = "ru";
+            description = "Russian Universal";
             languages = [ "rus" ];
             symbolsFile = ./universal-ru.xkb;
           };
