6d95d1906c600262b078a1ee4c3035e6e70d4580
Radimir Mikhailov <radimir.com@gmail.com>
2025-10-13 14:25:02 +0700

    Осуществлён переход на LTS-версию ядра Linux (#26)

--- parts/modules/base/boot/faster.nix
+++ parts/modules/base/boot/faster.nix
@@ -6,8 +6,8 @@
       # FIXME: next-release: в 25.11 планируется сделать по-умолчанию: https://github.com/NixOS/nixpkgs/pull/435781
       boot.initrd.systemd.enable = true;
 
-      # Время в секундах на ожидание выбора поколения NixOS (default = 5)
-      boot.loader.timeout = 1;
+      # Время в секундах на ожидание выбора поколения NixOS (default = 5, 0 - не показывать меню)
+      boot.loader.timeout = 0;
 
       # Не ждать при загрузке готовности сети
       systemd.services.NetworkManger-wait-online.wantedBy = lib.mkForce [];
