e9debcfbe614ac3f07791f3a76d9bb9166878241
Radimir Mikhailov <radimir.com@gmail.com>
2025-11-06 08:58:50 +0100

    print tuning

--- parts/flake/apps/starter.sh
+++ parts/flake/apps/starter.sh
@@ -43,6 +43,7 @@ main() {
   echo -e "$START_MSG"
 
   hostname=$(input_hostname)
+  echo
   set_github_token
 
   print_step_msg "validate GitHub token..."
@@ -139,7 +140,7 @@ generate_ssh_key() {
   if [[ -f $keyfile ]]
   then
     $sudo ssh-keygen -f "$keyfile" -c -C "$username@$hostname_new" -q > /dev/null
-    print_step_msg "\033[A\033[A... SSH key '$keyfile' already exists, updated key comment"
+    print_step_msg "... SSH key '$keyfile' already exists, updated key comment"
   else
     # Generate key pair without passphrase
     $sudo ssh-keygen -t ed25519 -N "" -f "$keyfile" -C "$username@$hostname_new"
