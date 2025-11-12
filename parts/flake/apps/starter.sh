9ec05d0fcd6fc7d5a35815469b03566b3adfe42e
Radimir Mikhailov <radimir.com@gmail.com>
2025-11-12 06:39:59 +0100

    refactor

--- parts/flake/apps/starter.sh
+++ parts/flake/apps/starter.sh
@@ -1,18 +1,17 @@
 # FIXME: remove
-set -o errexit
-set -o pipefail
+# set -o errexit
+# set -o pipefail
 
 GIT_DOTFILES_URL=github.com/radimih/nixdots
 # GIT_SECRETS_URL=github.com/radimih/nixdots-secrets
 HOME_DOTFILES_DIR=$HOME/1git/personal
 
 STARTER_PACKAGES="git vim"  # ВНИМАНИЕ! Предполагается, что бинарник у пакета = названию пакета
-STARTER_NIX_MODULE='
-{ pkgs, ... }: {
+STARTER_NIX_MODULE=\
+'{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [ '$STARTER_PACKAGES' ];
   nix.settings.experimental-features = [ "flakes" "nix-command" ];
-}
-'
+}'
 
 NIX_CONFIG_FILE=/etc/nix/nix.conf
 NIXOS_CONFIG_FILE=/etc/nixos/configuration.nix
@@ -33,17 +32,19 @@ ST_UNDERLINE='\033[4m'
 START_MSG="
 This script does the following:
 
-1. Generates host and user ${ST_BOLD}SSH keys${ST_REGULAR} if they do not exist
-
-2. Adds the ${ST_BOLD}user's public SSH key${ST_REGULAR} to ${ST_UNDERLINE}GitHub${ST_RESET} if it is not already added
-
-3. Clones dotfiles repo ${ST_UNDERLINE}$GIT_DOTFILES_URL${ST_RESET} into directory ${ST_DIM}$HOME_DOTFILES_DIR${ST_REGULAR}
-
-4. Prepares host folder in parts/hosts
-
-5. Updates the ${ST_BOLD}NixOS configuration file${ST_REGULAR} (${ST_DIM}$NIXOS_CONFIG_FILE${ST_REGULAR}):
+1. Updates the ${ST_BOLD}NixOS configuration file${ST_REGULAR} (${ST_DIM}${NIXOS_CONFIG_FILE}${ST_REGULAR}):
      - enables experimental features
-     - adds the ${ST_DIM}git${ST_REGULAR} and ${ST_DIM}vim${ST_REGULAR} programs to the system packages
+     - adds the ${ST_DIM}${STARTER_PACKAGES}${ST_REGULAR} programs to the system packages
+
+2. Generates host and user ${ST_BOLD}SSH keys${ST_REGULAR} if they do not exist
+
+3. Receives the ${ST_BOLD}GitHub token${ST_REGULAR} from the user and verifies it
+
+4. Adds the ${ST_BOLD}user's public SSH key${ST_REGULAR} to ${ST_UNDERLINE}GitHub${ST_RESET} if it is not already added
+
+5. Clones dotfiles repo ${ST_UNDERLINE}${GIT_DOTFILES_URL}${ST_RESET} into directory ${ST_DIM}${HOME_DOTFILES_DIR}${ST_REGULAR}
+
+6. Prepares host folder in parts/hosts
 
 Let's go!
 "
@@ -59,12 +60,10 @@ main() {
   hostname=$(input_hostname)
   echo
 
-  # FIXME: uncomment
-  # set_github_token
-  # validate_github_token
-  # generate_ssh_keys "$hostname"
-  # add_key_to_github "$hostname"
   update_system_config
+  generate_ssh_keys "$hostname"
+  verify_github_token
+  add_key_to_github "$hostname"
 
   echo -e "$FINISH_MSG"
 }
@@ -73,8 +72,7 @@ input_hostname() {
 
   local hostname_input=""
 
-  while true
-  do
+  while true; do
     read -r -e -p "Enter new hostname (only Latin letters, numbers and symbols '-', '_'): " -i "$hostname_input" hostname_input
     if [[ -z "$hostname_input" ]]; then continue; fi
     if [[ "$hostname_input" =~ ^[a-zA-Z0-9_-]+$ ]]; then break; fi
@@ -83,54 +81,66 @@ input_hostname() {
   echo "$hostname_input"
 }
 
-set_github_token() {
+update_system_config() {
 
-  while true
-  do
-    if [ ! -f "$TOKEN_FILE" ]
-    then
-      read -r -e -p "Enter GitHub token: " token
-      echo "$token" > "$TOKEN_FILE"
-    fi
+  print_step_msg "Updating NixOS configuration"
+  sudo --validate
+  echo
 
-    token=$(cat "$TOKEN_FILE")
-    hash=$(echo "$token" | sha256sum | awk '{print $1}')
-    hash_short=${hash:0:3}...${hash: -3}
+  print_line_msg "the following ${ST_DIM}${starter_file}${ST_REGULAR} module will be added to the NixOS configuration:"
+  echo
+  echo "$STARTER_NIX_MODULE"
+  pause
+  echo
+  enable_starter_module
 
-    echo -e "──────────────────────────────────────────────────────────────────────────"
-    echo -e "GitHub token stored in the ${ST_DIM}$TOKEN_FILE${ST_REGULAR} file:"
-    echo -e "  token: $token"
-    echo -e "  sha256sum: $hash_short"
-    echo -e "──────────────────────────────────────────────────────────────────────────"
+  if is_enabled_experimental_features && \
+     is_enabled_system_packages "$STARTER_PACKAGES"; then
+    print_line_msg "... ${ST_DIM}${STARTER_PACKAGES}${ST_REGULAR} system packages and ${ST_DIM}flakes nix-command${ST_REGULAR} experimental features already enabled"
+  else
+    print_line_msg "the command ${ST_DIM}sudo nixos-rebuild switch${ST_REGULAR} will be run to make the changes in the NixOS configuration take effect"
+    pause
+  fi
 
-    read -r -p "Is this token correct? (y/n): " answer
+  sudo nixos-rebuild switch
+  pause
+}
 
-    if [[ "$answer" =~ ^[Yy]$ ]]
-    then
-      break
-    else
-      read -r -e -i "$token" -p "Edit the token: " new_token
-      echo "$new_token" > "$TOKEN_FILE"
+enable_starter_module() {
+
+  local starter_file="$(dirname "$NIXOS_CONFIG_FILE")/starter.nix"
+
+  sudo echo "$STARTER_NIX_MODULE" > "$starter_file"
+
+  # Проверить, есть ли уже ./starter.nix в конфигурационном файле NixOS
+  if grep --silent --no-messages './starter.nix' "$NIXOS_CONFIG_FILE"; then return 0; fi
+
+  # Вставить строку '      ./starter.nix' в imports после ./hardware-configuration.nix
+  sed --in-place '/^\s*\.\/hardware-configuration\.nix\s*$/a\ \ \ \ \ \ .\/starter.nix' "$NIXOS_CONFIG_FILE"
+}
+
+# ВНИМАНИЕ! Проверяется только на одну экспериментальную функцию: flakes
+is_enabled_experimental_features() {
+
+  local regex_flakes='^[[:space:]]*(extra-)?experimental-features[[:space:]]*=[[:space:]]*([a-zA-Z-]+[[:space:]]+)*flakes([[:space:]]+[a-zA-Z-]+)*[[:space:]]*$'
+
+  grep --silent --no-messages -E "$regex_flakes" $NIX_CONFIG_FILE
+}
+
+# ВНИМАНИЕ! Работает только для пакетов, у которых бинарник называется
+# так же, как cам пакет
+is_enabled_system_packages() {
+
+  local packages="$1"
+  local file
+
+  for file in $packages; do
+    if [[ ! -e "/run/current-system/sw/bin/$file" ]]; then
+      return 1
     fi
   done
 
-  GITHUB_TOKEN=$(cat "$TOKEN_FILE")
-  export GITHUB_TOKEN
-}
-
-validate_github_token() {
-
-  print_step_msg "Validating GitHub token"
-
-  set +o errexit
-  if ! gh auth status;
-  then
-    print_error_msg "the GitHub token may have expired"
-    exit 1
-  fi
-  set -o errexit
-
-  pause
+  return 0
 }
 
 generate_ssh_keys() {
@@ -159,8 +169,7 @@ generate_ssh_key() {
 
   [[ -z "$sudo" ]] && username=$USER || username=host
 
-  if [[ -f $keyfile ]]
-  then
+  if [[ -f $keyfile ]]; then
     $sudo ssh-keygen -f "$keyfile" -c -C "$username@$hostname" -q > /dev/null
     print_line_msg "... SSH key ${ST_DIM}$keyfile${ST_REGULAR} already exists, updated key comment"
   else
@@ -168,6 +177,53 @@ generate_ssh_key() {
     $sudo ssh-keygen -t ed25519 -N "" -f "$keyfile" -C "$username@$hostname"
   fi
   echo
+}
+
+verify_github_token() {
+
+  print_step_msg "GitHub token verification"
+
+  while true; do
+    if [ ! -f "$TOKEN_FILE" ]; then
+      read -r -e -p "Enter GitHub token: " token
+      echo "$token" > "$TOKEN_FILE"
+    fi
+
+    token=$(cat "$TOKEN_FILE")
+    hash=$(echo "$token" | sha256sum | awk '{print $1}')
+    hash_short=${hash:0:3}...${hash: -3}
+
+    echo -e "──────────────────────────────────────────────────────────────────────────"
+    echo -e "GitHub token stored in the ${ST_DIM}$TOKEN_FILE${ST_REGULAR} file:"
+    echo -e "  token: $token"
+    echo -e "  sha256sum: $hash_short"
+    echo -e "──────────────────────────────────────────────────────────────────────────"
+
+    read -r -p "Is this token correct? (y/n): " answer
+
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+      break
+    else
+      read -r -e -i "$token" -p "Edit the token: " new_token
+      echo "$new_token" > "$TOKEN_FILE"
+    fi
+  done
+
+  GITHUB_TOKEN=$(cat "$TOKEN_FILE")
+  export GITHUB_TOKEN
+
+  check_github_token
+  pause
+}
+
+check_github_token() {
+
+  set +o errexit
+  if ! gh auth status; then
+    print_error_msg "the GitHub token may have expired"
+    exit 1
+  fi
+  set -o errexit
 }
 
 add_key_to_github() {
@@ -197,23 +253,19 @@ add_key_to_github() {
   pause
   echo
 
-  for key_type in authentication signing
-  do
+  for key_type in authentication signing; do
     # Получить имя ключа на GitHub по его публичной части. GitHub не позволяет
     # хранить один и тот же ключ под разными именами
     github_key_title=$(echo "$github_keys" | awk -v key="$new_key_pub" -v type="$key_type" '$3 == key && $6 == type { print $1; exit }')
 
     # Если на GitHub нет такого ключа
-    if [[ -z "$github_key_title" ]]
-    then
+    if [[ -z "$github_key_title" ]]; then
       # Если на GitHub есть другой ключ с таким именем (фактически происходит замена ключа)
-      if echo "$github_keys" | awk '{ print $1 }' | grep -q "$new_key_title";
-      then
+      if echo "$github_keys" | awk '{ print $1 }' | grep -q "$new_key_title"; then
         print_line_msg "... replacing user's public SSH key for ${ST_DIM}$key_type${ST_REGULAR}"
         remove_key_from_github "$new_key_title" "$key_type" "$github_keys"
       fi
-    elif [[ "$github_key_title" == "$new_key_title" ]]
-    then
+    elif [[ "$github_key_title" == "$new_key_title" ]]; then
       print_line_msg "... user public SSH key ${ST_BOLD}$new_key_title${ST_REGULAR} for ${ST_DIM}$key_type${ST_REGULAR} already exists"
       continue
     else
@@ -241,8 +293,7 @@ remove_key_from_github() {
 
   # К сожалению, signing-ключи нельзя удалять командой gh ssh-key delete, получаем ошибку
   # HTTP 404: Not Found (https://api.github.com/user/keys/ID) - не тот URI
-  if [[ "$key_type" == "signing" ]]
-  then
+  if [[ "$key_type" == "signing" ]]; then
     # https://docs.github.com/en/rest/users/ssh-signing-keys?apiVersion=2022-11-28#delete-an-ssh-signing-key-for-the-authenticated-user
     gh api \
       --method DELETE \
@@ -252,62 +303,6 @@ remove_key_from_github() {
   else
     gh ssh-key delete "$key_id" --yes
   fi
-}
-
-update_system_config() {
-
-  print_step_msg "Updating NixOS configuration"
-
-  print_line_msg "the following ${ST_DIM}${starter_file}${ST_REGULAR} module will be added to the NixOS configuration:"
-  echo "$STARTER_NIX_MODULE"
-  enable_starter_module
-
-  if is_enabled_experimental_features && \
-     is_enabled_system_packages "$STARTER_PACKAGES"
-  then
-    print_line_msg "... ${ST_DIM}${STARTER_PACKAGES}${ST_REGULAR} system packages and ${ST_DIM}flakes nix-command${ST_REGULAR} experimental features already enabled"
-  else
-    print_line_msg "... run the ${ST_DIM}sudo nixos-rebuild switch${ST_REGULAR} command to make the changes in the NixOS configuration take effect"
-    pause
-  fi
-
-  # FIXME: replace
-  # sudo nixos-rebuild switch
-  echo run sudo nixos-rebuild switch
-
-  pause
-}
-
-enable_starter_module() {
-
-  local starter_file="$(dirname "$NIXOS_CONFIG_FILE")/starter.nix"
-  echo $starter_file
-}
-
-# ВНИМАНИЕ! Проверяется только на одну экспериментальную функцию: flakes
-is_enabled_experimental_features() {
-
-  local regex_flakes='^[[:space:]]*(extra-)?experimental-features[[:space:]]*=[[:space:]]*([a-zA-Z-]+[[:space:]]+)*flakes([[:space:]]+[a-zA-Z-]+)*[[:space:]]*$'
-
-  # FIXME: remove
-  NIX_CONFIG_FILE=./nix.conf
-  grep --silent --no-messages -E "$regex_flakes" $NIX_CONFIG_FILE
-}
-
-# ВНИМАНИЕ! Работает только для пакетов, у которых бинарник называется
-# так же, как cам пакет
-is_enabled_system_packages() {
-
-  local packages="$1"
-  local file
-
-  for file in $packages; do
-    if [[ ! -e "/run/current-system/sw/bin/$file" ]]; then
-      return 1
-    fi
-  done
-
-  return 0
 }
 
 pause() {
