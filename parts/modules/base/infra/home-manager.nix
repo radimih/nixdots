257d15780a41be1a2d497fcebb01af51b4e73049
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-11 04:52:56 +0100

    Добавлен XDG-параметр в настройки для всех пользователей Home Manager

--- parts/modules/base/infra/home-manager.nix
+++ parts/modules/base/infra/home-manager.nix
@@ -22,10 +22,13 @@
             Home Manager в режиме NixOS-модуля устанавливает их автоматически:
             https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L53-L54
           */
+
+          home.preferXdgDirectories = true;
+
           # example: https://github.com/henrysipp/nix-setup/blob/48a93d0275eba0adf48977609fc100dce8f9b49c/modules/base/system/default.nix
           home.stateVersion = "25.05";  # TODO: stateVersion: 1) одинаково для всех пользователей? 2) вынести в глобальные константы?
 
-          # Let home Manager install and manage itself
+          # Let Home Manager install and manage itself
           programs.home-manager.enable = true;
         }
       ];
