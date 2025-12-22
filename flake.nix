2427aef83bc6fa486706bd0c173d715415a2aa68
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-22 12:49:11 +0100

    disable Lanzaboote

--- flake.nix
+++ flake.nix
@@ -29,16 +29,23 @@
 
     import-tree.url = "github:vic/import-tree";
 
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-      # рекомендуют сами разработчики lanzaboote (см. https://github.com/nix-community/lanzaboote/blob/master/flake.nix)
-      inputs.pre-commit-hooks-nix.follows = "";
-    };
+  # FIXME: перестал работать Lanzaboote
+    # lanzaboote = {
+    #   url = "github:nix-community/lanzaboote";
+    #   inputs.flake-parts.follows = "flake-parts";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   # рекомендуют сами разработчики lanzaboote (см. https://github.com/nix-community/lanzaboote/blob/master/flake.nix)
+    #   inputs.pre-commit-hooks-nix.follows = "";
+    # };
 
     stylix = {
       url = "github:nix-community/stylix/release-25.05";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    vaultix = {
+      url = "github:milieuim/vaultix";
       inputs.flake-parts.follows = "flake-parts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
