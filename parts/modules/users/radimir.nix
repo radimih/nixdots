85b65151c801c43535f8779a9330136d251998ce
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-26 14:50:28 +0100

    trial

--- parts/modules/users/radimir.nix
+++ parts/modules/users/radimir.nix
@@ -40,6 +40,10 @@ in
       host = osConfig.networking.hostName;
     in
     {
+      imports = [
+        inputs.agenix.homeManagerModules.default
+      ];
+
       home.file."hello-user.txt".text = "Hello, ${user.name}! host = ${host}.";
 
       home.file.trial = {
