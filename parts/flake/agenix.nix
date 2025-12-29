fd35f9d3cd3d879d29016387493bc8392836d284
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-29 10:24:00 +0100

    trial

--- parts/flake/agenix.nix
+++ parts/flake/agenix.nix
@@ -1,17 +1,51 @@
-# https://github.com/oddlama/agenix-rekey
+# # https://github.com/oddlama/agenix-rekey
+# {
+#   inputs,
+#   self,
+#   ...
+# }:
+# {
+#   imports = [
+#     # flake-parts.agenix-rekey: https://flake.parts/options/agenix-rekey.html
+#     inputs.agenix-rekey.flakeModule
+#   ];
+
+#   # flake.agenix-rekey = inputs.agenix-rekey.configure {
+#   #   userFlake = self;
+#   #   inherit (self) nixosConfigurations;
+#   # };
+# }
 {
   inputs,
   self,
   ...
 }:
 {
-  imports = [
-    # flake-parts.agenix-rekey: https://flake.parts/options/agenix-rekey.html
-    inputs.agenix-rekey.flakeModule
-  ];
+  flake.agenix-rekey = inputs.agenix-rekey.configure {
+    userFlake = self;
+    inherit (self) nixosConfigurations;
+  };
 
-  # flake.agenix-rekey = inputs.agenix-rekey.configure {
-  #   userFlake = self;
-  #   inherit (self) nixosConfigurations;
-  # };
+  perSystem =
+    {
+      inputs',
+      pkgs,
+      ...
+    }:
+    {
+      devshells.default = {
+        packages = [
+          inputs'.agenix-rekey.packages.default
+          pkgs.age-plugin-yubikey
+        ];
+        env = [
+          {
+            # Automatically add rekeyed secrets to git without
+            # requiring `agenix rekey -a`
+            name = "AGENIX_REKEY_ADD_TO_GIT";
+            value = true;
+          }
+        ];
+      };
+    };
 }
