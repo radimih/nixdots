cbf7a169c8dffd203f7d3bc4d8e6b52817293dd1
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-29 10:25:23 +0100

    trial

--- parts/flake/agenix.nix
+++ parts/flake/agenix.nix
@@ -26,26 +26,26 @@
     inherit (self) nixosConfigurations;
   };
 
-  perSystem =
-    {
-      inputs',
-      pkgs,
-      ...
-    }:
-    {
-      devshells.default = {
-        packages = [
-          inputs'.agenix-rekey.packages.default
-          pkgs.age-plugin-yubikey
-        ];
-        env = [
-          {
-            # Automatically add rekeyed secrets to git without
-            # requiring `agenix rekey -a`
-            name = "AGENIX_REKEY_ADD_TO_GIT";
-            value = true;
-          }
-        ];
-      };
-    };
+  # perSystem =
+  #   {
+  #     inputs',
+  #     pkgs,
+  #     ...
+  #   }:
+  #   {
+  #     devshells.default = {
+  #       packages = [
+  #         inputs'.agenix-rekey.packages.default
+  #         pkgs.age-plugin-yubikey
+  #       ];
+  #       env = [
+  #         {
+  #           # Automatically add rekeyed secrets to git without
+  #           # requiring `agenix rekey -a`
+  #           name = "AGENIX_REKEY_ADD_TO_GIT";
+  #           value = true;
+  #         }
+  #       ];
+  #     };
+  #   };
 }
