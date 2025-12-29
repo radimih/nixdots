d65e95953296b6be99b8e30bdc8f21ede1b8fbd4
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-29 11:33:28 +0100

    trial

--- parts/modules/base/infra/agenix.nix
+++ parts/modules/base/infra/agenix.nix
@@ -41,7 +41,6 @@
       imports = [
         inputs.agenix.homeManagerModules.default
         # inputs.agenix-rekey.homeManagerModules.default
-        inputs.agenix-rekey.homeManagerModules.agenix-rekey
       ];
 
       # age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
