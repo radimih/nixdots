24adb474927ec028e821ea3f18c2deab7608b027
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-29 07:05:45 +0100

    trial

--- parts/modules/base/infra/agenix.nix
+++ parts/modules/base/infra/agenix.nix
@@ -1,9 +1,15 @@
 # https://github.com/oddlama/agenix-rekey
 {
   inputs,
+  self,
   ...
 }:
 {
+  flake.agenix-rekey = inputs.agenix-rekey.configure {
+    userFlake = self;
+    inherit (self) nixosConfigurations;
+  };
+
   flake.modules.nixos.base =
     { ... }:
     let
