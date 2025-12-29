e77673149266509111212796c2c98e1d571d5bae
Radimir Mikhailov <radimir.com@gmail.com>
2025-12-29 13:35:53 +0100

    todos

--- parts/flake/meta.nix
+++ parts/flake/meta.nix
@@ -7,3 +7,5 @@
     type = with lib.types; lazyAttrsOf anything;
   };
 }
+# TODO: пример использования: https://github.com/LilDojd/rhizome/blob/master/modules/meta/owner.nix
+#                             https://github.com/LilDojd/rhizome/blob/master/modules/git/defaults.nix
--- parts/modules/base/infra/home-manager.nix
+++ parts/modules/base/infra/home-manager.nix
@@ -5,6 +5,9 @@
   ...
 }:
 {
+  # TODO: home-manager refactor
+  #       Пример: https://github.com/alex007sirois/nix-config/blob/main/modules/home/home-manager.nix
+  #       То есть у пользователя у хоста всегда импортировать base
   flake.modules.nixos.base = {
 
     imports = [
