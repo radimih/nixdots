7c0637a0bd55a1b66b7c8816530c92545ed3a756
Radimir Mikhailov <radimir.com@gmail.com>
2026-01-21 13:19:14 +0100

    return

--- parts/modules/users/radimir.nix
+++ parts/modules/users/radimir.nix
@@ -40,7 +40,7 @@ in
       home.file."hello-user.txt".text = "Hello, ${user.name}! host = ${host}.";
 
       home.file.trial = {
-        source = config.age.secrets.trial.file;
+        source = config.age.secrets.trial.path;
         target = "super-secret-file.txt";
       };
 
