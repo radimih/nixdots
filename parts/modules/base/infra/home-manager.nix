f791969648e1edc3b8d1d6caf6919918c7a9901a
Radimir Mikhailov <radimir.com@gmail.com>
2026-01-29 05:42:18 +0100

    initial

--- parts/modules/base/infra/home-manager.nix
+++ parts/modules/base/infra/home-manager.nix
@@ -5,35 +5,43 @@
   ...
 }:
 {
-  # TODO: home-manager refactor
-  #       Пример: https://github.com/alex007sirois/nix-config/blob/main/modules/home/home-manager.nix
-  #       То есть у пользователя у хоста всегда импортировать base
   flake.modules.nixos.base = {
 
     imports = [
       inputs.home-manager.nixosModules.home-manager
     ];
 
+    # https://nix-community.github.io/home-manager/nixos-options.xhtml
+
     home-manager = {
       extraSpecialArgs = {
         secrets = inputs.secrets;
       };
-      sharedModules = [
-        {
-          /*
-            Нет необходимости устанавливать параметры home.username|homeDirectory, так как
-            Home Manager в режиме NixOS-модуля устанавливает их автоматически:
-            https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L53-L54
-          */
-
-          home.preferXdgDirectories = true;
-
-          # Let Home Manager install and manage itself
-          programs.home-manager.enable = true;
-        }
-      ];
       useGlobalPkgs = true;
       useUserPackages = true;
+    };
+  };
+
+  flake.modules.homeManager.base = {
+
+    # https://nix-community.github.io/home-manager/options.xhtml
+
+    /*
+      Нет необходимости устанавливать параметры home.username|homeDirectory, так как
+      Home Manager в режиме NixOS-модуля устанавливает их автоматически:
+      https://github.com/nix-community/home-manager/blob/master/nixos/common.nix#L53-L54
+    */
+
+    home.preferXdgDirectories = true;
+    programs.home-manager.enable = true;
+
+    services = {
+      home-manager.autoExpire = {
+        enable = true;
+        frequency = "weekly";
+        store.cleanup = false;  # не запускать nix-collect-garbage. Его запуск настраивается в ./nix.nix
+        timestamp = "-3 days";  # удалять поколения Home Manager (generations) старше 3-х дней
+      };
     };
   };
 }
