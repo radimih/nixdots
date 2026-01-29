bb6f42738f372aab70e9c9d8024d9ea4c1687ad0
Radimir Mikhailov <radimir.com@gmail.com>
2026-01-29 10:19:50 +0100

    Возвращён упреждающий запрос root-прав

--- starter.sh
+++ starter.sh
@@ -116,6 +116,8 @@ update_system_config() {
   echo
   print_line_msg "--> The command ${ST_DIM}nixos-rebuild switch --sudo${ST_REGULAR} will be run to make the changes in the NixOS configuration take effect"
   pause
+  sudo --validate
+  echo
   enable_starter_module
   nixos-rebuild switch --sudo
 }
