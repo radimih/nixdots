83e905a178a599a15a5c03b9d844dcbc730d3334
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-01 12:30:38 +0000

    add user name

--- parts/modules/vpn/it2g.nix
+++ parts/modules/vpn/it2g.nix
@@ -31,6 +31,7 @@
                 service-type = "org.freedesktop.NetworkManager.openvpn";
                 ta = "${secrets}/it2g/vpn-ta.pem";
                 ta-dir = "1";
+                user-name = "mikhailovrv"
               };
             };
           };
