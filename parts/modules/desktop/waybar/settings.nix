8c92484622ed9ab92d82bc75b5439da41df9d44e
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-02 05:39:44 +0100

    remove network from waybar

--- parts/modules/desktop/waybar/settings.nix
+++ parts/modules/desktop/waybar/settings.nix
@@ -28,17 +28,10 @@
         # ─────────────────────────────────────────────────────────┤ Right │
         modules-right = [
           "tray"
-          "network"
           "niri/language"
           "clock#local"
           "clock#msk"
         ];
-
-        network = {
-          format-disconnected = "⚠";
-          format-ethernet = "";
-          format-wifi = "";
-        };
 
 	      "niri/language" = {
           format = "{short}";
