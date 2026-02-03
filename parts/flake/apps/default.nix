55a23d4d72a66d413c5b632de6841a3031b5d868
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-03 07:20:28 +0100

    return

--- parts/flake/apps/default.nix
+++ parts/flake/apps/default.nix
@@ -3,6 +3,33 @@
   perSystem =
     { pkgs, ... }:
     {
-      apps.rekey = import ./rekey.nix inputs pkgs;
+      apps.app1 = {
+        meta.description = "app1";
+        type = "app";
+        program = pkgs.writeShellApplication {
+          name = "rekey.sh";
+          runtimeInputs = with pkgs; [
+            expect
+          ];
+          text = ''
+            echo Hello! It's app1
+            echo ${inputs.secrets}/master-key.age
+          '';
+        };
+      };
+
+      apps.app2 = {
+        meta.description = "app2";
+        type = "app";
+        program = pkgs.writeShellApplication {
+          name = "app2.sh";
+          runtimeInputs = with pkgs; [
+            expect
+          ];
+          text = ''
+            echo It's app2!
+          '';
+        };
+      };
     };
 }
