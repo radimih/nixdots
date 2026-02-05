d420674d0f68104bc75117f540ccfcb5ebb6da1d
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-05 07:21:14 +0100

    Убрано окончательно использование home-manager.sharedModules

--- parts/modules/desktop/xdg.nix
+++ parts/modules/desktop/xdg.nix
@@ -1,31 +1,31 @@
 {
   flake.modules.nixos.desktop = {
 
-    home-manager.sharedModules = [
-      {
-        home.preferXdgDirectories = true;
-        xdg = {
-          enable = true;
-          userDirs = {
-            enable = true;
-            createDirectories = true;
-          };
-          # Оставить только documents и download
-          userDirs = {
-            desktop = null;
-            music = null;
-            pictures = null;
-            publicShare = null;
-            templates = null;
-            videos = null;
-          };
-        };
-      }
-    ];
-
     xdg.portal = {
       enable = true;
       xdgOpenUsePortal = true;
+    };
+  };
+
+  flake.modules.homeManager.desktop = {
+
+    home.preferXdgDirectories = true;
+
+    xdg = {
+      enable = true;
+      userDirs = {
+        enable = true;
+        createDirectories = true;
+      };
+      # Оставить только documents и download
+      userDirs = {
+        desktop = null;
+        music = null;
+        pictures = null;
+        publicShare = null;
+        templates = null;
+        videos = null;
+      };
     };
   };
 }
