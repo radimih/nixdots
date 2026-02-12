d35acdb569d136d6f259df2031986acef68e1c8b
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-12 12:21:19 +0100

    remove yazi level

--- parts/modules/programs/yazi.nix
+++ parts/modules/programs/yazi.nix
@@ -14,21 +14,19 @@
       programs.yazi = {
         enable = true;
         settings = {
-          yazi = {
-            mgr = {
-              linemode = "size";
-              ratio = [
-                1
-                4
-                4
-              ];
-              show_hidden = true;
-              show_symlink = true;
-              sort_by = "natural";
-              sort_dir_first = true;
-              sort_reverse = false;
-              sort_sensitive = false;
-            };
+          mgr = {
+            linemode = "size";
+            ratio = [
+              1
+              4
+              4
+            ];
+            show_hidden = true;
+            show_symlink = true;
+            sort_by = "natural";
+            sort_dir_first = true;
+            sort_reverse = false;
+            sort_sensitive = false;
           };
         };
       };
