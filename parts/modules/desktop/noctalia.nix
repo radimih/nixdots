c18c68677746ae9f8469b1abb1fffba2a0cd6515
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-16 09:57:07 +0100

    fix settings level

--- parts/modules/desktop/noctalia.nix
+++ parts/modules/desktop/noctalia.nix
@@ -13,63 +13,47 @@
 
       programs.noctalia-shell = {
         enable = true;
-
-        bar = {
-          left = [
-            {
-              id = "Workspace";
-            }
-          ];
-          center = [
-            {
-              id = "ActiveWindow";
-              maxWidth = 200;
-            }
-          ];
-          right = [
-            {
-              id = "Tray";
-            }
-            {
-              id = "ControlCenter";
-            }
-            {
-              id = "NotificationHistory";
-            }
-            {
-              id = "KeyboardLayout";
-              showIcon = false;
-            }
-            {
-              id = "Clock";
-              formatHorizontal = "HH:mm";
-            }
-            {
-              id = "SessionMenu";
-            }
-          ];
-        };
-
-        desktopWidgets.enabled = false;
-
-        dock.enabled = false;
-
-        hooks.screenLock = "niri msg action switch-layout 0";
-
-        location.name = "Kemerovo";
-
-        notifications = {
-          enableKeyboardLayoutToast = false;
-        };
-
-        sessionMenu = {
-          enableCountdown = false;
-          largeButtonsLayout = "grid";
-        };
-
         systemd.enable = true;
 
-        wallpaper.enabled = false;
+        settings = {
+          bar = {
+            left = [
+              {
+                id = "Workspace";
+              }
+            ];
+            center = [
+              {
+                id = "ActiveWindow";
+                maxWidth = 200;
+              }
+            ];
+            right = [
+              {
+                id = "Tray";
+              }
+              {
+                id = "ControlCenter";
+              }
+              {
+                id = "NotificationHistory";
+              }
+              {
+                id = "KeyboardLayout";
+                showIcon = false;
+              }
+              {
+                id = "Clock";
+                formatHorizontal = "HH:mm";
+              }
+              {
+                id = "SessionMenu";
+              }
+            ];
+          };
+
+          wallpaper.enabled = false;
+        };
       };
     };
 }
