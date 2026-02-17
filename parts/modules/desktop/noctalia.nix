8c09ea5339e77578734bae94d105a24ef3643e6f
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-17 11:01:26 +0100

    recommended setup niri

--- parts/modules/desktop/noctalia.nix
+++ parts/modules/desktop/noctalia.nix
@@ -11,15 +11,24 @@
         inputs.noctalia.homeModules.default
       ];
 
-      programs.niri.settings.binds = {
-        "Mod+A" = {
-          action.spawn = [ "noctalia-shell" "ipc" "call" "launcher" "toggle" ];
-          repeat = false;
-          hotkey-overlay.title = "App Launcher";
+      programs.niri.settings = {
+
+        binds = {
+          "Mod+A" = {
+            action.spawn = [ "noctalia-shell" "ipc" "call" "launcher" "toggle" ];
+            repeat = false;
+            hotkey-overlay.title = "App Launcher";
+          };
+        };
+
+        # https://docs.noctalia.dev/getting-started/compositor-settings/niri/
+        debug = {
+          honor-xdg-activation-with-invalid-serial = [];
         };
       };
 
       programs.noctalia-shell = {
+
         enable = true;
         systemd.enable = true;
 
