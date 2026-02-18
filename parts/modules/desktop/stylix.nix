ee03ad08012be170f19148bdb57b18bf2699c705
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-18 12:36:00 +0100

    try blueish theme

--- parts/modules/desktop/stylix.nix
+++ parts/modules/desktop/stylix.nix
@@ -17,7 +17,7 @@
 
         # https://github.com/tinted-theming/schemes
         # base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/ashes.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/blueish.yaml";
 
         # https://github.com/ful1e5/Bibata_Cursor
         cursor = {
