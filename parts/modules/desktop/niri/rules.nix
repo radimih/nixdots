58f32a8937cc757130a198e0887ad88963f28ccb
Radimir Mikhailov <radimir.com@gmail.com>
2026-02-27 11:29:55 +0100

    fix

--- parts/modules/desktop/niri/rules.nix
+++ parts/modules/desktop/niri/rules.nix
@@ -44,7 +44,7 @@
           };
           default-column-width.proportion = 0.5;
           default-window-height.proportion = 0.4;
-          opacity = 1;
+          opacity = 1.0;
           open-floating = true;
         }
       ];
