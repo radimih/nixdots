c6359d4ae98d7fe77395b508f12f4dd3009290de
Radimir Mikhailov <radimir.com@gmail.com>
2026-03-05 12:16:21 +0100

    wl-copy

--- parts/modules/programs/satty.nix
+++ parts/modules/programs/satty.nix
@@ -1,22 +1,23 @@
 # Screenshot annotation tool: https://github.com/Satty-org/Satty
 {
-  flake.modules.homeManager.satty = {
+  flake.modules.homeManager.satty =
+    { lib, pkgs, ... }:
 
-    programs.satty = {
-      enable = true;
-      settings = {
-        general = {
-          actions-on-escape = [ "exit" ];
-          annotation-size-factor = 1;
-          disable-notifications = true;
-          early-exit = true;
-          fullscreen = true;
-          initial-tool = "crop";
-          primary-highlighter = "freehand";
-          # TODO: next-release: новый параметр в 0.20.1, default = 1.1
-          # zoom-factor = 1.0;
+      programs.satty = {
+        enable = true;
+        settings = {
+          general = {
+            actions-on-escape = [ "exit" ];
+            annotation-size-factor = 1;
+            copy-command = "${lib.getExe' pkgs.wl-clipboard "wl-copy"}";
+            disable-notifications = true;
+            early-exit = true;
+            fullscreen = true;
+            initial-tool = "crop";
+            primary-highlighter = "freehand";
+            # TODO: next-release: новый параметр в 0.20.1, default = 1.1
+            # zoom-factor = 1.0;
+          };
         };
       };
-    };
-  };
 }
