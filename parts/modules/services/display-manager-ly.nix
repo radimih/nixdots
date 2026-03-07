0cf8b42e7c86ca6ed8b62d787f918027b513c624
Radimir Mikhailov <radimir@mail.ru>
2026-03-07 16:36:50 +0700

    comment asterisk

--- parts/modules/services/display-manager-ly.nix
+++ parts/modules/services/display-manager-ly.nix
@@ -8,7 +8,7 @@
       # https://github.com/fairyglade/ly/blob/master/res/config.ini
       settings = {
         animation = "matrix";
-        asterisk = "•";
+        # asterisk = "•";
         brightness_down_key = null;
         brightness_up_key = null;
         clear_password = true;
