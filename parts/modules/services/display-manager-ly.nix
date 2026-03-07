2dfafdb10827b21c8aebe1b8fff74d1f37e61901
Radimir Mikhailov <radimir@mail.ru>
2026-03-07 16:48:12 +0700

    japan

--- parts/modules/services/display-manager-ly.nix
+++ parts/modules/services/display-manager-ly.nix
@@ -8,11 +8,13 @@
       # https://github.com/fairyglade/ly/blob/master/res/config.ini
       settings = {
         animation = "matrix";
-        asterisk = "0x2022";  # код UTF-32 символа •
+        asterisk = "0x2022";  # код UTF-32 символа '•'
         brightness_down_key = null;
         brightness_up_key = null;
         clear_password = true;
         clock = "%d.%m.%Y %T";
+        cmatrix_min_codepoint = "0x3000";
+        cmatrix_max_codepoint = "0x30FF";
         vi_mode = true;
       };
       x11Support = false;
