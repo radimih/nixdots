e93002b35425e4f5cec339eaa83ae800ea44ae38
Radimir Mikhailov <radimir@mail.ru>
2026-03-07 16:25:32 +0700

    initial

--- parts/modules/services/display-manager-ly.nix
+++ parts/modules/services/display-manager-ly.nix
@@ -4,8 +4,14 @@
 
     services.displayManager.ly = {
       enable = true;
+      # Описание параметров и значения по-умолчанию:
+      # https://github.com/fairyglade/ly/blob/master/res/config.ini
       settings = {
         animation = "matrix";
+        asterisk = "•";
+        brightness_down_key = null;
+        brightness_up_key = null;
+        clear_password = true;
         clock = "%d.%m.%Y %T";
         vi_mode = true;
       };
