4e8ad1aa7c4b1650f85eec8c6e62b1df58638a67
Radimir Mikhailov <radimir@mail.ru>
2026-03-08 10:58:19 +0700

    version up

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell?ref=v4.6.1";
+      url = "github:noctalia-dev/noctalia-shell?ref=v4.6.4";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
