947af91c8dab98108516259dc7e57a59750bdfda
Radimir Mikhailov <radimir@mail.ru>
2026-03-14 22:19:37 +0700

    Замена стандартного виджета ВПН на плагин (#50)

--- parts/modules/services/network-manager-applet.nix
+++ parts/modules/services/network-manager-applet.nix
@@ -1,8 +1,15 @@
 {
-  flake.modules.homeManager.desktop = {
+  flake.modules.homeManager.desktop =
+    { pkgs, ... }:
+    {
+      # Чтобы через PATH были доступны все утилиты пакета. Например, nm-connection-editor
+      # используется в одном из плагинов Noctalia Shell
+      home.packages = [
+        pkgs.networkmanagerapplet
+      ];
 
-    services = {
-      network-manager-applet.enable = true;
+      services = {
+        network-manager-applet.enable = true;
+      };
     };
-  };
 }
