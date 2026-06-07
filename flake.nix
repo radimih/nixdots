76bea5903c001ee50fb6e6e55a0d5cf54e05d1d6
Radimir Mikhailov <radimir@mail.ru>
2026-06-07 18:14:05 +0700

    trial noctalia-qs

--- flake.nix
+++ flake.nix
@@ -62,6 +62,7 @@
       url = "github:noctalia-dev/noctalia-shell?ref=v4.7.6";
       # inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.noctalia-qs.follows = "nixpkgs";
     };
 
     # --- other
