0be1504e96721c49e4786555a28de152afd894fd
Radimir Mikhailov <radimir.com@gmail.com>
2026-08-18 18:51:48 +0700

    Update comment

--- parts/modules/desktop/_noctalia-v4.nix
+++ parts/modules/desktop/_noctalia-v4.nix
@@ -9,22 +9,7 @@
     {
       programs.noctalia-shell = {
 
-        enable = true;
-        package = pkgs.noctalia-shell;
-        systemd.enable = true;
-
-        # Использовать "родную" тему от Noctalia, вместо темы из Stylix
-        colors =
-          let
-            # Взять из темы цветовую схему dark и убрать цвета для терминала
-            # JSON-файлы тем: https://github.com/noctalia-dev/noctalia/tree/legacy-v4/Assets/ColorScheme
-            colorsTheme =
-              builtins.readFile "${inputs.noctalia.outPath}/Assets/ColorScheme/Tokyo-Night/Tokyo-Night.json"
-              |> builtins.fromJSON
-              |> (attrs: attrs.dark)
-              |> (attrs: builtins.removeAttrs attrs ["terminal"]);
-          in
-          lib.mkForce colorsTheme;
+        # FIXME: Параметры, котором не найдены аналоги в Noctalia v4
 
         settings = {
           appLauncher = {
