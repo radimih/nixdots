6265afe89176b6a4cd9a2fbed0c0c096a6143d86
Radimir Mikhailov <radimir.com@gmail.com>
2026-08-18 11:38:43 +0700

    launcher hot key & disable suspend mode

--- parts/modules/desktop/niri/binds.nix
+++ parts/modules/desktop/niri/binds.nix
@@ -28,7 +28,7 @@
         "Mod+Shift+7".action.show-hotkey-overlay = { };
 
         "Mod+A" = {
-          action.spawn = [ "noctalia-shell" "ipc" "call" "launcher" "toggle" ];
+          action.spawn = [ "noctalia" "msg" "panel-toggle" "launcher" ]
           repeat = false;
           hotkey-overlay.title = "Open App Launcher";
         };
