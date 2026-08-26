8360782a9182df49c153ec581bfa594deb493257
Radimir Mikhailov <radimir@mail.ru>
2026-08-26 18:20:00 +0700

    Добавлено пояснение в time.nix

--- parts/modules/base/time.nix
+++ parts/modules/base/time.nix
@@ -1,6 +1,9 @@
 {
   flake.modules.nixos.base = {
 
+    # Список NTP-серверов. По-умолчанию используется как службой
+    # systemd-timesyncd, так и службой chronyd
+
     networking.timeServers = [
       "0.nixos.pool.ntp.org"
       "0.ru.pool.ntp.org"
